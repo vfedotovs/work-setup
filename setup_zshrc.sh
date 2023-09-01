41dfafff06632716a928eae05e3bba419296e396
Valentins Fedotovs <39173395+vfedotovs@users.noreply.github.com>
2023-09-02 00:44:00 +0100

    Update setup_zshrc.sh

--- setup_zshrc.sh
+++ setup_zshrc.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
 # Update package list
-sudo apt update
+sudo apt update -y
 
-# Install Zsh
-sudo apt install zsh git
+# nvim 0.9 install 
+mkdir -p ~/.local/bin/
+wget wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz /tmp 
 
-echo "Zsh installed successfully."
+cp /tmp/nvim-linux64.tar.gz ~/.local/bin/
+cd ~/.local/bin/
+tar xzvf nvim-linux64.tar.gz
+
+ln -s ./nvim-linux64/bin/nvim ./nvim
+export PATH="$HOME/.local/bin:$PATH"
+
+# Install zsh
+sudo apt install zsh tmux -y
+
+echo "NVIM, Zsh, tmux  installed successfully."
 
 # Clone the repository (change <repository_url> to your actual repository URL)
-git clone <repository_url> ~/zshrc_repo
 
+
+# TODO:
+# Add download config files for nvim, zsh, tmux 
 # Update the .zshrc file on the new PC (replace with your customizations)
-cp ~/zshrc_repo/.zshrc ~
+# cp ~/zshrc_repo/.zshrc ~
 
 # Apply the changes to the current terminal session
-source ~/.zshrc
+# source ~/.zshrc
 
 echo "Zsh configuration has been updated and changes applied."
