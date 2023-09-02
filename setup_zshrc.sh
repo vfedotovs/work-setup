aa8f37a65e1ae9bf3a44dfe3b675cbe0f6231342
Valentins Fedotovs <39173395+vfedotovs@users.noreply.github.com>
2023-09-02 13:52:20 +0100

    Add wget install and versions for wget and nvim

--- setup_zshrc.sh
+++ setup_zshrc.sh
@@ -3,16 +3,24 @@
 # Update package list
 sudo apt update -y
 
-# nvim 0.9 install 
+sudo apt install wget -y
+wget --version | grep GNU
+
+# nvim 0.9 install
 mkdir -p ~/.local/bin/
-wget wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz /tmp 
+wget wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz /tmp
 
 cp /tmp/nvim-linux64.tar.gz ~/.local/bin/
-cd ~/.local/bin/
+cd ~/.local/bin/ || {
+	echo "Error could not cd ~/.local/bin/"
+	exit 1
+}
 tar xzvf nvim-linux64.tar.gz
 
 ln -s ./nvim-linux64/bin/nvim ./nvim
 export PATH="$HOME/.local/bin:$PATH"
+
+nvim --version
 
 # Install packages
 # ripgrep required for nvim to search files and grep text
@@ -20,19 +28,17 @@ sudo apt install zsh tmux fzf ripgrep -y
 
 echo "NVIM, Zsh, tmux  installed successfully."
 
-
 # Tmux config import
-mkdir -p ~/work-tools  && cd ~/work-tools    
-git clone https://github.com/vfedotovs/tools.git .    
+mkdir -p ~/work-tools && cd ~/work-tools
+git clone https://github.com/vfedotovs/tools.git .
 cp tmux.conf ~/.tmux.conf
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm   # Before install tmux plugins clone repo
-tmux source-file ~/.tmux.conf   
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm # Before install tmux plugins clone repo
+tmux source-file ~/.tmux.conf
 
 echo "To install tmux plugins: tmux and C-a + i  # This step will install tmux pluggins"
 
-
 # TODO:
-# Add download config files for nvim, zsh, tmux 
+# Add download config files for nvim, zsh, tmux
 # Update the .zshrc file on the new PC (replace with your customizations)
 # cp ~/zshrc_repo/.zshrc ~
 
